1d94b6a5c8cc11239fb8592404d9416da4b62072
zjones2142 <zachjones2142@gmail.com>
2024-04-05 16:09:29 -0500

    zj - removal of test main, testing event listener

--- PluginProject/src/main/java/edu/mu/PluginProject/Plugin.java
+++ PluginProject/src/main/java/edu/mu/PluginProject/Plugin.java
@@ -1,18 +1,29 @@
 package edu.mu.PluginProject;
 
 import java.util.logging.Logger;
+
+import org.bukkit.event.*;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Plugin extends JavaPlugin
+public class Plugin extends JavaPlugin implements Listener
 {
   private static final Logger LOGGER=Logger.getLogger("PluginProject");
 
+  @Override
   public void onEnable()
   {
     LOGGER.info("PluginProject enabled");
-    System.out.println("PLUGIN WORKS!!");
+    getServer().getPluginManager().registerEvents(this, this);
   }
-
+  
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent event)
+  {
+	  System.out.println("Player has joined");
+  }
+  
+  @Override
   public void onDisable()
   {
     LOGGER.info("PluginProject disabled");
