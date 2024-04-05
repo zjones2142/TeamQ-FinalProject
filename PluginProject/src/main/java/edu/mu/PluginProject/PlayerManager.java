85735321744bc325db31891025839ebe3a087453
zjones2142 <zachjones2142@gmail.com>
2024-04-05 17:44:48 -0500

    zj - player coord functionality first steps (displays in chat currently)

---
+++ PluginProject/src/main/java/edu/mu/PluginProject/PlayerManager.java
@@ -0,0 +1,16 @@
+package edu.mu.PluginProject;
+
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
+
+public class PlayerManager {
+	
+	//public ArrayList<Player> playerList;
+	public ArrayList<Player> playerList;
+
+	public PlayerManager() {
+		this.playerList = new ArrayList<Player>();
+	}
+
+}
--- PluginProject/src/main/java/edu/mu/PluginProject/Plugin.java
+++ PluginProject/src/main/java/edu/mu/PluginProject/Plugin.java
@@ -2,14 +2,18 @@ package edu.mu.PluginProject;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.*;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin extends JavaPlugin implements Listener
 {
-  private static final Logger LOGGER=Logger.getLogger("PluginProject");
-
+  private static final Logger LOGGER = Logger.getLogger("PluginProject");
+  public PlayerManager playerManager = new PlayerManager();
+  
   @Override
   public void onEnable()
   {
@@ -20,7 +24,25 @@ public class Plugin extends JavaPlugin implements Listener
   @EventHandler
   public void onPlayerJoin(PlayerJoinEvent event)
   {
-	  System.out.println("Player has joined");
+	  Player p = event.getPlayer();
+	  this.playerManager.playerList.add(p);
+  }
+  
+  @EventHandler
+  public void onPlayerMove(PlayerMoveEvent event)
+  {
+	  Player p = event.getPlayer();
+	  Location pTo = event.getTo();
+	  Location pFrom = event.getFrom();
+	  
+	  int xLoc = p.getLocation().getBlockX();
+	  int yLoc = p.getLocation().getBlockY();
+	  int zLoc = p.getLocation().getBlockZ();
+	  
+	  if ((pTo.getBlockX() != pFrom.getBlockX()) || (pTo.getBlockZ() != pFrom.getBlockZ()) || (pTo.getBlockY() != pFrom.getBlockY())) 
+	  {
+		  p.sendMessage("X: "+xLoc+" | Y: " +yLoc+" | Z: "+zLoc);
+	  }
   }
   
   @Override
