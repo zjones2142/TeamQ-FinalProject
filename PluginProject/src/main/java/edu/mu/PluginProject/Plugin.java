333e224d2c11c6e22310196bee5761d4a57d95e1
zjones2142 <zachjones2142@gmail.com>
2024-04-06 02:04:23 -0500

    zj - player coords now display on action bar

--- PluginProject/src/main/java/edu/mu/PluginProject/Plugin.java
+++ PluginProject/src/main/java/edu/mu/PluginProject/Plugin.java
@@ -2,12 +2,14 @@ package edu.mu.PluginProject;
 
 import java.util.logging.Logger;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.player.*;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class Plugin extends JavaPlugin implements Listener
 {
@@ -39,9 +41,11 @@ public class Plugin extends JavaPlugin implements Listener
 	  int yLoc = p.getLocation().getBlockY();
 	  int zLoc = p.getLocation().getBlockZ();
 	  
+	  String text = "X: "+xLoc+" | Y: " +yLoc+" | Z: "+zLoc;
+	  
 	  if ((pTo.getBlockX() != pFrom.getBlockX()) || (pTo.getBlockZ() != pFrom.getBlockZ()) || (pTo.getBlockY() != pFrom.getBlockY())) 
 	  {
-		  p.sendMessage("X: "+xLoc+" | Y: " +yLoc+" | Z: "+zLoc);
+		  p.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(text));
 	  }
   }
   
