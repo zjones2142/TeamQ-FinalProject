69c293ecb2d40411f3570ef90efcdc31c49dc938
zjones2142 <zachjones2142@gmail.com>
2024-04-08 13:14:16 -0500

    zj - player coordinates now display in a built in scoreboard

--- PluginProject/src/main/java/edu/mu/PluginProject/Plugin.java
+++ PluginProject/src/main/java/edu/mu/PluginProject/Plugin.java
@@ -1,26 +1,80 @@
 package edu.mu.PluginProject;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.logging.Logger;
 
-import org.bukkit.Location;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.player.*;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.scoreboard.*;
 
 public class Plugin extends JavaPlugin implements Listener
 {
   private static final Logger LOGGER = Logger.getLogger("PluginProject");
   public PlayerManager playerManager = new PlayerManager();
+  private final Map<UUID, Scoreboard> boards = new HashMap<>();
+  
+  public Scoreboard createScoreboard(Player p) 
+  {
+	    Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+	    Objective objective = scoreboard.registerNewObjective("test", "dummy", "test", RenderType.INTEGER);
+	    
+	    this.boards.put(p.getUniqueId(), scoreboard);
+	    
+	    objective.setDisplayName("Coordinates");
+	    objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+
+	    Score x = objective.getScore("X: ");
+	    Score y = objective.getScore("Y: ");
+	    Score z = objective.getScore("Z: ");
+
+	    x.setScore(0);
+	    y.setScore(0);
+	    z.setScore(0);
+
+	    return scoreboard;
+	}
+  
+  public int[] getPlayerLocation(Player p) 
+  {
+	  int[] text = {p.getLocation().getBlockX(), p.getLocation().getBlockY(), p.getLocation().getBlockZ()};
+	  return text;
+  }
+  
+  public void updatePlayerBoard(Player p)
+  {
+	  Scoreboard sb = this.boards.get(p.getUniqueId());
+	  Objective ob = sb.getObjective("test");
+	  int[] coords = getPlayerLocation(p);
+	  
+	  Score x = ob.getScore("X: ");
+	  Score y = ob.getScore("Y: ");
+	  Score z = ob.getScore("Z: ");
+	  
+	  x.setScore(coords[0]);
+	  y.setScore(coords[1]);
+	  z.setScore(coords[2]);
+  }
   
   @Override
   public void onEnable()
   {
     LOGGER.info("PluginProject enabled");
     getServer().getPluginManager().registerEvents(this, this);
+    Bukkit.getPluginManager().registerEvents(this, this);
+    
+//    getServer().getScheduler().runTaskTimer(this, new Runnable() {
+//		@Override
+//		public void run() {
+//		    for (FastBoard board : Plugin.this.boards.values()) {
+//		        updateBoard(board);
+//		    }
+//		}
+//	}, 0, 5);
   }
   
   @EventHandler
@@ -28,25 +82,22 @@ public class Plugin extends JavaPlugin implements Listener
   {
 	  Player p = event.getPlayer();
 	  this.playerManager.playerList.add(p);
+	  p.setScoreboard(createScoreboard(p));
+  }
+  
+  @EventHandler
+  public void onPlayerQuit(PlayerQuitEvent event)
+  {
+	  Player p = event.getPlayer();
+	  LOGGER.info(p.getDisplayName()+" has quit");
+
   }
   
   @EventHandler
   public void onPlayerMove(PlayerMoveEvent event)
   {
 	  Player p = event.getPlayer();
-	  Location pTo = event.getTo();
-	  Location pFrom = event.getFrom();
-	  
-	  int xLoc = p.getLocation().getBlockX();
-	  int yLoc = p.getLocation().getBlockY();
-	  int zLoc = p.getLocation().getBlockZ();
-	  
-	  String text = "X: "+xLoc+" | Y: " +yLoc+" | Z: "+zLoc;
-	  
-	  if ((pTo.getBlockX() != pFrom.getBlockX()) || (pTo.getBlockZ() != pFrom.getBlockZ()) || (pTo.getBlockY() != pFrom.getBlockY())) 
-	  {
-		  p.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(text));
-	  }
+	  updatePlayerBoard(p);
   }
   
   @Override
