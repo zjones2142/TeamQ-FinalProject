e69bc2e4bedd3a325941e240b638fe1ed0417662
zjones2142 <zachjones2142@gmail.com>
2024-04-21 20:58:44 -0500

    zj - custom chest ui class implementing

---
+++ PluginProject/src/main/java/edu/mu/PluginProject/CoordinateUI.java
@@ -0,0 +1,79 @@
+package edu.mu.PluginProject;
+
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class CoordinateUI implements Listener{
+	private final Inventory inv;
+
+	public CoordinateUI() 
+	{
+		this.inv = Bukkit.createInventory(null, 9, "Example");
+		
+		initInv();
+	}
+	
+	public void initInv()
+	{
+		this.inv.addItem(createGuiItem(Material.DIAMOND_SWORD, "Example Sword", "text"));
+	}
+	
+	public void openInventory(final HumanEntity ent) 
+	{
+        ent.openInventory(inv);
+    }
+	
+	protected ItemStack createGuiItem(final Material material, final String name, final String... lore)
+	{
+        final ItemStack item = new ItemStack(material, 1);
+        final ItemMeta meta = item.getItemMeta();
+
+        // Set the name of the item
+        meta.setDisplayName(name);
+
+        // Set the lore of the item
+        meta.setLore(Arrays.asList(lore));
+
+        item.setItemMeta(meta);
+
+        return item;
+    }
+	
+	@EventHandler
+	public void onInventoryClick(final InventoryClickEvent e)
+	{
+        if (!e.getInventory().equals(inv)) return;
+
+        e.setCancelled(true);
+
+        final ItemStack clickedItem = e.getCurrentItem();
+
+        // verify current item is not null
+        if (clickedItem == null || clickedItem.getType().isAir()) return;
+
+        final Player p = (Player) e.getWhoClicked();
+
+        // Using slots click is a best option for your inventory click's
+        p.sendMessage("You clicked at slot " + e.getRawSlot());
+    }
+	
+	@EventHandler
+    public void onInventoryClick(final InventoryDragEvent e) 
+	{
+        if (e.getInventory().equals(inv)) 
+        {
+          e.setCancelled(true);
+        }
+    }
+}
