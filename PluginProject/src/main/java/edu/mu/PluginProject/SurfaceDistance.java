e26ae11726c801cc5e6803f4726af7b677fea2fd
Rafael Bratfich <90302625+brxtz@users.noreply.github.com>
2024-05-05 15:33:32 -0500

    rb - First implementation of Distance To Surface Implemented Distance To Suraface feature, needs to go through testing and I will still add extra methods.

---
+++ PluginProject/src/main/java/edu/mu/PluginProject/SurfaceDistance.java
@@ -0,0 +1,40 @@
+package edu.mu.PluginProject;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SurfaceDistance extends JavaPlugin implements Listener{
+    
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getLogger().info("SurfaceDistancePlugin enabled");
+    }
+
+    @Override
+    public void onDisable(){
+        getLogger().info("SurfaceDistancePlugin disabled");
+    }    
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event){ //calculates and updates player distance to surface based off player movement
+        Player player = event.getPlayer();
+        Location currentLocation = player.getLocation();
+        Location highestBlock = player.getWorld().getHighestBlockAt(currentLocation.getBlockX(), currentLocation.getBlockZ()).getLocation();
+
+        //check player's y-level
+        int distanceToSurface = highestBlock.getBlockY() - currentLocation.getBlockY();
+        
+        if(distanceToSurface >= 0){
+            player.sendMessage("Distance to the surface: " + distanceToSurface + " blocks.");
+        } else {
+            player.sendMessage("You are already at surface level");
+        }
+    }
+}
